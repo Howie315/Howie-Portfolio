c14fb0b099c9d9afaa38e6aff1cb2ac899cc2dff
Howie315 <nguyen.howie2010@gmail.com>
2023-06-29 13:37:30 -0700

    Finished setting up some stuff

--- src/App.js
+++ src/App.js
@@ -1,22 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./components/Home";
+import About from "./components/About";
+import Experience from "./components/Experience";
 
 function App() {
+  const [currentSection, setCurrentSection] = useState("home");
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const home = document.getElementById("home").getBoundingClientRect();
+      const about = document.getElementById("about").getBoundingClientRect();
+      const experience = document
+        .getElementById("experience")
+        .getBoundingClientRect();
+      const halfWindowHeight = window.innerHeight / 2;
+
+      if (home.top <= halfWindowHeight && home.bottom >= halfWindowHeight) {
+        setCurrentSection("home");
+      } else if (
+        about.top <= halfWindowHeight &&
+        about.bottom >= halfWindowHeight
+      ) {
+        setCurrentSection("about");
+      } else if (
+        experience.top <= halfWindowHeight &&
+        experience.bottom >= halfWindowHeight
+      ) {
+        setCurrentSection("experience");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="navbar">
+          <a href="#home" className={currentSection === "home" ? "active" : ""}>
+            Home
+          </a>
+          <a
+            href="#about"
+            className={currentSection === "about" ? "active" : ""}
+          >
+            About
+          </a>
+          <a
+            href="#experience"
+            className={currentSection === "experience" ? "active" : ""}
+          >
+            Experience
+          </a>
+        </div>
+        <Home />
+        <About />
+        <Experience />
       </header>
     </div>
   );
