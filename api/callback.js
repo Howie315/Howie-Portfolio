18940ad744599ef944e521094391e1b7330f083e
Howie315 <nguyen.howie2010@gmail.com>
2023-07-02 14:03:17 -0700

    spotify player

---
+++ api/callback.js
@@ -0,0 +1,29 @@
+const axios = require("axios");
+
+module.exports = async (req, res) => {
+  const { code } = req.query;
+
+  try {
+    const response = await axios.post(
+      "https://accounts.spotify.com/api/token",
+      null,
+      {
+        params: {
+          grant_type: "authorization_code",
+          code,
+          redirect_uri: "http://localhost:3000/api/callback", // note the /api/callback route here
+          client_id: "87fcc6d0cb494536a8112b362e87d18c",
+          client_secret: "37c0cd373f1c4b598233c2cdaf853bf0",
+        },
+      }
+    );
+
+    const { access_token, refresh_token } = response.data;
+
+    // You would then typically store these tokens in your app's state and use them to make requests to the Spotify API
+    res.status(200).json({ access_token, refresh_token });
+  } catch (error) {
+    console.error("Error during token acquisition", error);
+    res.status(500).json({ error: "Error during token acquisition" });
+  }
+};
--- src/App.js
+++ src/App.js
@@ -3,11 +3,37 @@ import "./App.css";
 import Home from "./components/Home";
 import About from "./components/About";
 import Experience from "./components/Experience";
+import { FaSpotify } from "react-icons/fa";
+
 import pdfFile from "./HowieNguyen.pdf"; // Import your PDF file
+import SpotifyPlayer from "react-spotify-web-playback";
+import axios from "axios";
 
 function App() {
   const [currentSection, setCurrentSection] = useState("home");
+  const [accessToken, setAccessToken] = useState(null);
+  const [trackUri, setTrackUri] = useState(
+    "spotify:playlist:06kWMlOMwbfRzNbmizxRIz?si=7a1ba407bcce42fa"
+  );
+  const [play, setPlay] = useState(false);
   const sections = ["home", "about", "experience"];
+
+  const handleSpotifyLogin = () => {
+    // Define these values for your application
+    const clientId = "87fcc6d0cb494536a8112b362e87d18c";
+    const redirectUri = encodeURIComponent(
+      "https://howie-portfolio.vercel.app/api/callback"
+    );
+    const scopes = encodeURIComponent(
+      "streaming user-read-email user-read-private"
+    );
+
+    // Create the authorization URL
+    const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&scope=${scopes}`;
+
+    // Redirect the user to the authorization URL
+    window.location.href = authUrl;
+  };
 
   useEffect(() => {
     const handleScroll = () => {
@@ -35,6 +61,16 @@ function App() {
     };
   }, []);
 
+  useEffect(() => {
+    axios
+      .get("/api/callback")
+      .then((response) => {
+        setAccessToken(response.data.access_token);
+      })
+      .catch((error) => {
+        console.error("Error fetching access token", error);
+      });
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
@@ -58,7 +94,19 @@ function App() {
           <a href={pdfFile} download className="download-link button">
             Resume
           </a>
+
+          <button onClick={handleSpotifyLogin}>Login to Spotify</button>
+
+          <button onClick={() => setPlay(!play)}>
+            {play ? "Pause" : "Play"}
+          </button>
+          <SpotifyPlayer
+            token={accessToken}
+            uris={play ? [trackUri] : []}
+            play={play}
+          />
         </div>
+
         <div id="home">
           <Home />
         </div>
